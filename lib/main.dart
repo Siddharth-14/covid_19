d3353c71e6567e978f7b5f9d9707bbb59cee25b6
Siddharth Goradia <siddharth.sg14@gmail.com>
2020-04-06 19:38:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+void main() {
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull("https://api.rootnet.in/covid19-in/stats/latest"),
+        headers: {
+          "Accept": "application/json"
+        }
+    );
+
+    this.setState(() {
+      data = json.decode(response.body);
+    });
+    print(data[1]["summary"]);
+
+    return "Success!";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new Card(
+            child: new Text(data[index]["data"]),
+          );
+        },
+      ),
+    );
+  }
+}
